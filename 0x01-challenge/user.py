70cfa4b0eaf5132c5f4c31c199948bfad3ff9dad
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-10 16:21:04 +0100

    user model fix

---
+++ 0x01-challenge/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 
+User class
+"""
+
+class User:
+    """ User class """
+
+    def __init__(self):
+        self.__email = None
+
+    @property
+    def email(self):
+        """ Get the email address """
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        """ Set the email address """
+        if not isinstance(value, str):
+            raise TypeError("email must be a string")
+        self.__email = value
+
+if __name__ == "__main__":
+    u = User()
+    u.email = "john@snow.com"
+    print(u.email)
